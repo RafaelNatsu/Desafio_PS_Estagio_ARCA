24adc879a05a1de75952dcd174c4bccf2e0c3810
RafaelNatsu <rafaelnatsu99@gmail.com>
2023-11-18 11:17:49 -0300

    feat: initial commit

---
+++ app/application/controllers/Dashboard.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+#[\AllowDynamicProperties]
+
+class Dashboard extends CI_Controller {
+
+	public function index()
+	{
+		$data['title'] = 'ARCA';
+
+		$this->load->view('templates/header');
+		$this->load->view('templates/nav-top');
+		$this->load->view('pages/dashboard',$data);
+		$this->load->view('templates/js');
+		$this->load->view('templates/footer');
+	}
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Desafio PS Estágio ARCA
+
+Este é um projeto para o desafio do Processo Seletivo da empresa: ARCA
+
+# Requirements
+- Docker
+
+# Running the app
+```bash
+# inicia aplicação
+$ docker compose up -d
+
+# finaliza
+$ docker compose down
+
+```
